492b4a5d0fe7fcdabafd7a11db7c4a50f54062bb
Lontor <ilyassakaev@gmail.com>
2025-03-05 21:52:56 +0300

    feat(core): add core interfaces and structs

--- internal/core/core.go
+++ internal/core/core.go
@@ -1,1 +1,23 @@
 package core
+
+type Reference struct {
+	Title   string
+	Authors []string
+}
+
+type Parser interface {
+	Parse(raw string) (Reference, error)
+}
+
+type APIClient interface {
+	Validate(ref Reference) (bool, error)
+}
+
+type Core struct {
+	parser  Parser
+	clients []APIClient
+}
+
+func New(parser Parser, clients []APIClient) *Core {
+	return &Core{parser: parser, clients: clients}
+}
