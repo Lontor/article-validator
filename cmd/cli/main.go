0b47efbbd40dcf993d286be7e3a3a1bcd4d212b6
Lontor <ilyassakaev@gmail.com>
2025-03-05 23:12:56 +0300

    feat(cli): initialize core with parser and API client

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/Lontor/article-validator/internal/apis"
+	"github.com/Lontor/article-validator/internal/core"
+	"github.com/Lontor/article-validator/internal/parser"
+)
+
+func main() {
+	p := parser.New()
+	client := apis.New()
+	core := core.New(p, []core.APIClient{client})
+
+	valid, _ := core.Validate("Hanke M. On the shape derivative of polygonal inclusions in the conductivity problem")
+	fmt.Printf("Valid: %v\n", valid)
 }
