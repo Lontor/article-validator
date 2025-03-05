26b84cad61d2714ed85d199f941a60d2b93f18d1
Lontor <ilyassakaev@gmail.com>
2025-03-05 22:01:02 +0300

    feat(parser): add basic parser structure

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,1 +1,13 @@
 package parser
+
+import "github.com/Lontor/article-validator/internal/core"
+
+type DefaultParser struct{}
+
+func New() *DefaultParser {
+	return &DefaultParser{}
+}
+
+func (p *DefaultParser) Parse(raw string) (core.Reference, error) {
+	return core.Reference{Title: raw}, nil
+}
