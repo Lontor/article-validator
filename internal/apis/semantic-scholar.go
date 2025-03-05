97f87eb4deaf21487e8af6333da8869bc626fb55
Lontor <ilyassakaev@gmail.com>
2025-03-05 22:04:56 +0300

    feat(apis): add Semantic Scholar client stub

--- internal/apis/semantic-scholar.go
+++ internal/apis/semantic-scholar.go
@@ -1,1 +1,13 @@
 package apis
+
+import "github.com/Lontor/article-validator/internal/core"
+
+type SemanticScholarClient struct{}
+
+func New() *SemanticScholarClient {
+	return &SemanticScholarClient{}
+}
+
+func (c *SemanticScholarClient) Validate(ref core.Reference) (bool, error) {
+	return true, nil
+}
