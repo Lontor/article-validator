32d0dfa97f86800ea147e9d4917fa605471cf2bc
Lontor <ilyassakaev@gmail.com>
2025-03-06 20:22:17 +0300

    feat(parser): implement basic parsing

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,6 +1,16 @@
 package parser
 
-import "github.com/Lontor/article-validator/internal/core"
+import (
+	"errors"
+	"regexp"
+	"strings"
+
+	"github.com/Lontor/article-validator/internal/core"
+)
+
+var (
+	ErrInvalidReference = errors.New("invalid reference: authors or title not found")
+)
 
 type DefaultParser struct{}
 
@@ -9,5 +19,36 @@ func New() *DefaultParser {
 }
 
 func (p *DefaultParser) Parse(raw string) (core.Reference, error) {
-	return core.Reference{Title: raw}, nil
+	ref := core.Reference{}
+
+	authorBlocksRegex := regexp.MustCompile(`(\p{Lu}+\p{Ll}*[\s.,]+(\p{Lu}[.,]\s*){1,3}(,\s*)?)+`)
+	authorBlocksMatches := authorBlocksRegex.FindAllString(raw, -1)
+	surnamesRegex := regexp.MustCompile(`\pL{2,}`)
+
+	for _, a := range authorBlocksMatches {
+		surnames := surnamesRegex.FindAllString(a, -1)
+		ref.Authors = append(ref.Authors, surnames...)
+	}
+
+	titlePart := raw
+	if len(authorBlocksMatches) > 0 {
+		titlePart = raw[len(authorBlocksMatches[0]):]
+	}
+
+	splitChars := []string{"/", "–", "."}
+	for _, sep := range splitChars {
+		if idx := strings.Index(titlePart, sep); idx > 0 {
+			titlePart = titlePart[:idx]
+			break
+		}
+	}
+
+	titleRegex := regexp.MustCompile(`\p{Lu}.*\p{L}`)
+	ref.Title = strings.TrimSpace(titleRegex.FindString(titlePart))
+
+	if len(ref.Title) == 0 || len(ref.Authors) == 0 {
+		return ref, ErrInvalidReference
+	}
+
+	return ref, nil
 }
