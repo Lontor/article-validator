465a6fcb4c20b07c5bf12bbf48eb6c02845126b5
Lontor <ilyassakaev@gmail.com>
2025-03-06 22:53:02 +0300

    feat(apis): implement initial Semantic Scholar client validation

--- internal/apis/semantic-scholar.go
+++ internal/apis/semantic-scholar.go
@@ -1,13 +1,96 @@
 package apis
 
-import "github.com/Lontor/article-validator/internal/core"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
 
-type SemanticScholarClient struct{}
+	"github.com/Lontor/article-validator/internal/core"
+)
 
-func New() *SemanticScholarClient {
-	return &SemanticScholarClient{}
+type author struct {
+	AuthorID string `json:"authorId"`
+	Name     string `json:"name"`
+}
+
+type paper struct {
+	PaperID    string   `json:"paperId"`
+	Title      string   `json:"title"`
+	Authors    []author `json:"authors"`
+	MatchScore float64  `json:"matchScore"`
+}
+
+type matchResponse struct {
+	Data []paper `json:"data"`
+}
+
+type SemanticScholarClient struct {
+	endpoint   string
+	maxRetries int
+}
+
+func New(endpoint string, maxRetries int) *SemanticScholarClient {
+	return &SemanticScholarClient{endpoint: endpoint, maxRetries: maxRetries}
 }
 
 func (c *SemanticScholarClient) Validate(ref core.Reference) (bool, error) {
+	params := url.Values{}
+	params.Set("query", fmt.Sprintf(`"%s"`, ref.Title))
+	params.Set("fields", "title,authors.name")
+
+	u, err := url.Parse(c.endpoint)
+	if err != nil {
+		return false, err
+	}
+
+	u.RawQuery = params.Encode()
+
+	var resp *http.Response
+
+	for retries := 0; retries < c.maxRetries; retries++ {
+		resp, err = http.Get(u.String())
+		if err != nil {
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		if resp.StatusCode == http.StatusTooManyRequests {
+			resp.Body.Close()
+			time.Sleep(10 * time.Second)
+			continue
+		}
+		break
+	}
+
+	if err != nil {
+		return false, err
+	}
+
+	if resp.StatusCode == http.StatusNotFound {
+		return false, nil
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return false, fmt.Errorf("HTTP error %d: %s", resp.StatusCode, resp.Status)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		return false, err
+	}
+
+	var data matchResponse
+	if err := json.Unmarshal(body, &data); err != nil {
+		return false, err
+	}
+
+	if len(data.Data) == 0 {
+		return false, nil
+	}
+
 	return true, nil
 }
